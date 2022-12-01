dbc35838bf91b2599596f4e8dd82010a373e606c
Adama Fofana <mafahfof55@gmail.com>
2022-12-01 22:08:28 +0100

    app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
-import Header from './mainComponents/Header'
 import Home from './pages/Home'
 import Portfolio from './pages/Portfolio'
 import CV from './pages/CV'
 import Contact from './pages/Contact'
+import Layout from './layout/Layout/Layout'
+import './styles/Theme.css'
 
 function App() {
   return (
-    <div className="App">
+    <div className="App back-color-bleu">
       <Router>
-        <Header/>
+        <Layout>
         <Routes>
             <Route path="/" element={<Home />}/>
             <Route path="portfolio/*" element={<Portfolio />}/>
             <Route path="cv/*" element={<CV />}/>
             <Route path="contact/*" element={<Contact />}/>
-        </Routes>        
-      </Router>
-      
+        </Routes> 
+        </Layout>               
+      </Router>      
     </div>
   );
 }

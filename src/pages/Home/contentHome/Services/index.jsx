4742fd420853bdd35fc7df035d58b7647b2e2b98
Adama Fofana <mafahfof55@gmail.com>
2022-12-09 04:46:36 +0100

    modification home

--- src/pages/Home/contentHome/Services/index.jsx
+++ src/pages/Home/contentHome/Services/index.jsx
@@ -1,29 +1,38 @@
 import React from "react"
-import Logotype from '../../../../assets/imageService/logotype.png'
-import Affiche from '../../../../assets/imageService/affiche.png'
-import CarteVisite from '../../../../assets/imageService/carteVisite.png'
+import ServicesData from "../../../../assets/Datas/ServicesData"
 import  './Services.css'
 
 function Services (){
     return(
         <div>
-            <div className="sl-services-header">
-                <h2> Conception logo, affiche et carte de visite  </h2>
-            </div>
-            <div className="sl-services-content">
-                <p>
-                    Je conçois des logos, affiches publicitaires, cartes de visites et cartes d'invitations.
-                    <br/>J'accompagne mes clients à choisir leurs logos, 
-                    à structurer leurs pages réseaux sociaux ou des 
-                    techniques pour attirer plus de clients. 
-                </p> 
-                <div className="sl-image-concep"> 
-                    <img src={Logotype} alt=" exemple de logo Bay Makup" />
-                    <img src={Affiche} alt=" exemple d'affiche publicitaire"/>
-                    <img src={CarteVisite} alt=" exemple de carte de visite"/>
-                </div>
-                                 
-            </div>
+            {
+                ServicesData.map((index) => {
+                    return(
+                        <div key = {index.id}>
+                            <div className="sl-services-header">
+                                <h2> {index.titre}</h2>
+                            </div>
+                            <div className="sl-services-content">
+                                <p>
+                                    {index.contenu} 
+                                </p> 
+                                <div className="sl-image-concep"> 
+                                    {index.Exemple.map((content, indexe) => {
+                                        return (
+                                        
+                                             <img  key = {indexe} src= {content}alt=" exemple de logo Bay Makup" />
+                                        
+                                        )
+                                    })}                                            
+                                </div>
+                                                
+                            </div>
+
+                        </div>
+                    )
+                })
+            }
+            
         </div>
     )
 }

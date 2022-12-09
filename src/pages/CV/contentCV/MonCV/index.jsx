79fd995e2fef76508c7e8a1b500003eff5220732
Adama Fofana <mafahfof55@gmail.com>
2022-12-09 04:46:02 +0100

    modification protfolio

--- src/pages/CV/contentCV/MonCV/index.jsx
+++ src/pages/CV/contentCV/MonCV/index.jsx
@@ -3,7 +3,8 @@ import React from "react"
 
 function MonCV () {
     return (
-        <p> engagement </p>
+    <>
+    </>
     )
 }
 
--- src/pages/Portfolio/contentPortfolio/Experiences/index.jsx
+++ src/pages/Portfolio/contentPortfolio/Experiences/index.jsx
@@ -1,32 +1,43 @@
 import React from "react"
-import ImCtcase from '../../../../assets/imageExperience/imCtcase.png'
+import ExperiencesData from "../../../../assets/Datas/ExperiencesData"
 import './Experiences.css'
 
 function Experiences() {
     return (
         <div >
-            <div className="sl-petit-barre">
-            </div>
-            <h2 className="sl-formation-titre"> Ingénieur électronique</h2>
-            <div className="sl-cadre-experience">
-                <div className="sl-content-experience">
-                    <p className="sl-entreprise-experience">Startup Ctcase</p>
-                    <p className="sl-lieu-periode">Drancy, France | Avril - Novembre 2021</p>
-                    <p className="sl-description-experience">
-                    Mission:
-                    <br/>Membre fondateur,
-                    Chargé d’effectuer l’inventaire des matériels et passer la commande,
-                    Mise en place des tests et les soudures électroniques.
-                    A l’issue de cette expérience je suis capable d’établir le cahier de charge pour un projet de taille moyenne représentant les fonctionnalités et évaluer le coup budgetaire.
-                    <br/>Environnement techniques :
-                    <br/>Tinkercad 
-                    Proteus
-                    </p>
-                </div>
-                <div className="sl-ulstration-imageExp">
-                    <img src= {ImCtcase} alt=" Equipe de Ctcase" />
-                </div>
-            </div>
+            {
+                ExperiencesData.map((index) => {
+                    return (
+                        <div key={index.id}>
+                            <div className="sl-petit-barre"></div>
+                            <h2 className="sl-formation-titre">{index.poste}</h2>
+                            <div className="sl-cadre-experience">
+                                <div className="sl-content-experience">
+                                    <p className="sl-entreprise-experience">{index.entreprise}</p>
+                                    <p className="sl-lieu-periode">{index.date_lieu}</p>
+                                    <p className="sl-description-experience">
+                                        {
+                                            index.contenu.map((content, indexe) => {
+                                                return (
+                                                    <>
+                                                        <br key = {indexe}/> {content}
+                                                    </>
+                                                
+                                                )
+                                            })
+                                        }
+                                    </p>
+                                    
+                                </div>
+                                <div className="sl-ulstration-imageExp">
+                                    <img src= {index.ulisimage} alt=" ulistration-images" />
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })
+
+            }
             
         </div>
     )
